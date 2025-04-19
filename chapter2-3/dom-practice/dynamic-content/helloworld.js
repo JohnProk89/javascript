59b18c7b12056c121ac0b45950baca0ce63f05cf
Giannis Prokopiou <johnprok89@gmail.com>
2025-04-19 05:34:48 +0300

    helloworld exercise

--- chapter2-3/dom-practice/dynamic-content/helloworld.js
+++ chapter2-3/dom-practice/dynamic-content/helloworld.js
@@ -1,7 +1,13 @@
-const container = document.querySelector('.container')
+const container1 = document.querySelector('.container1')
+container1.innerHTML = "<p>Hello World !!!</p>"
 const container2 = document.querySelector('.container2')
-const p = document.createElement('p')
-p.textContent = "Hello World !"
-container.innerHTML = "<p>Hello World !!!</p>"
 container2.innerHTML += "<p>Says Hello World !!!</p>"
-container.appendChild(p)
+const container3 = document.querySelector('.container3')
+const p3 = document.createElement('p')
+p3.textContent = "Hello World !"
+container3.appendChild(p3)
+const container4 = document.querySelector('.container4')
+const p4 = document.createElement('p')
+const text4 = "Hello Coding Factory!"
+p4.append(text4)
+container4.appendChild(p4)
