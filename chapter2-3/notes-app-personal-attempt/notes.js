4b87d452ff1842603823a93898794805897271fc
Giannis Prokopiou <johnprok89@gmail.com>
2025-04-22 20:14:33 +0300

    First JS attempts

---
+++ chapter2-3/notes-app-personal-attempt/notes.js
@@ -0,0 +1,8 @@
+function addNote () {
+    const addButton = document.querySelector('.note-adder')
+    addButton.addEventListener('click', function() {
+        console.log("Hello Button");
+    })
+}
+
+console.log("I am a message being printed")
