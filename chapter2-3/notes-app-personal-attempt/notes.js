b5ee7ab13db13e8ca6dbd4578df89cd7c410b79d
Giannis Prokopiou <johnprok89@gmail.com>
2025-04-23 19:08:22 +0300

    check note, delete note first attempts

--- chapter2-3/notes-app-personal-attempt/notes.js
+++ chapter2-3/notes-app-personal-attempt/notes.js
@@ -2,13 +2,14 @@ const addBtn = document.getElementById('submitNote');
 addBtn.addEventListener('click', function() {
     const container = document.querySelector('.main');
     const noteBlock = document.createElement('div');
+    const id = 1;
     const noteText = document.getElementById('inputText').value
     noteBlock.classList.add('note');
     container.appendChild(noteBlock);
     noteBlock.innerHTML = `
             <div class="left-part">
                 <div class="checker">  
-                    <button class="">
+                    <button id="checkNote" onclick="checkNote()">
                     </button> 
                 </div>
                 <div class="content">
@@ -16,9 +17,24 @@ addBtn.addEventListener('click', function() {
                 </div>
             </div>
             <div class="deleter">
-                <button class="">
+                <button id="deleteNote" onclick="deleteNote()">
                     X
                 </button> 
             </div>
         `;
+    // noteBlock.id = id++;
 });
+
+function checkNote() {
+    const btn = document.getElementById('checkNote');
+    btn.innerHTML = `
+        <i class="fa-solid fa-check"></i>
+    ` 
+}
+
+function deleteNote() {
+    // const currentId = '';
+    // const note = document.getElementById('id');
+    // note.remove();
+    console.log("Trying to remove note...")
+}
