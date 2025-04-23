18ce48f4789b8634c23fb14698df365ab70655d3
Giannis Prokopiou <johnprok89@gmail.com>
2025-04-23 21:36:00 +0300

    Basic id generation and tracking

--- chapter2-3/notes-app-personal-attempt/notes.js
+++ chapter2-3/notes-app-personal-attempt/notes.js
@@ -1,15 +1,17 @@
 const addBtn = document.getElementById('submitNote');
-addBtn.addEventListener('click', function() {
+let noteId = 0;
+// let checkId = 0;
+addBtn.addEventListener('click', function createNote() {
     const container = document.querySelector('.main');
     const noteBlock = document.createElement('div');
-    const id = 1;
     const noteText = document.getElementById('inputText').value
     noteBlock.classList.add('note');
+    noteBlock.id = noteId++;
     container.appendChild(noteBlock);
     noteBlock.innerHTML = `
             <div class="left-part">
                 <div class="checker">  
-                    <button id="checkNote" onclick="checkNote()">
+                    <button id="checkNote" onclick="checkNote(${noteBlock.id})">
                     </button> 
                 </div>
                 <div class="content">
@@ -22,14 +24,17 @@ addBtn.addEventListener('click', function() {
                 </button> 
             </div>
         `;
-    // noteBlock.id = id++;
+    console.log(noteBlock.id);
 });
 
-function checkNote() {
-    const btn = document.getElementById('checkNote');
+function checkNote(id) {
+    const note = document.getElementById('id');
+    // const btn = document.getElementById('checkNote');
+    const btn = note.children[0].children[0].children[0];
     btn.innerHTML = `
         <i class="fa-solid fa-check"></i>
     ` 
+    console.log("Checked note", id)
 }
 
 function deleteNote() {
