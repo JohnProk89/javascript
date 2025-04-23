a51d40b8cee94089c63f643cf3f6b21944e9e7a7
Giannis Prokopiou <johnprok89@gmail.com>
2025-04-23 16:08:51 +0300

    Basic formatting new note

--- chapter2-3/notes-app-personal-attempt/notes.js
+++ chapter2-3/notes-app-personal-attempt/notes.js
@@ -1,20 +1,24 @@
-function addNote () {
-        const container = document.querySelector('.main');
-        const noteBlock = document.createElement('div');
-        const noteText = document.getElementById('inputText').value
-        noteBlock.classList.add('note');
-        container.appendChild(noteBlock);
-        noteBlock.innerHTML = `
+const addBtn = document.getElementById('submitNote');
+addBtn.addEventListener('click', function() {
+    const container = document.querySelector('.main');
+    const noteBlock = document.createElement('div');
+    const noteText = document.getElementById('inputText').value
+    noteBlock.classList.add('note');
+    container.appendChild(noteBlock);
+    noteBlock.innerHTML = `
             <div class="left-part">
-                <div class=".checker">   
-                    Check Button
+                <div class="checker">  
+                    <button class="">
+                    </button> 
                 </div>
-                <div class=".content">
+                <div class="content">
                 ${noteText}
                 </div>
             </div>
-            <div class=".deleter">
-                Delete Button
+            <div class="deleter">
+                <button class="">
+                    X
+                </button> 
             </div>
         `;
-    }
+});
